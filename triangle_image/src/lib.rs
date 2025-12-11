fbdce796c5214eb58cf34e81b980734e5707ac50
Ivor Wanders <ivor@iwanders.net>
2025-12-10 20:56:41 -0500

    Triangle on the image!

--- triangle_image/src/lib.rs
+++ triangle_image/src/lib.rs
@@ -7,6 +7,7 @@
 // Lets just start with tutorial 2, and pick from tutorial 1 as we see fit.
 
 use anyhow::Context;
+use log::*;
 use std::path::Path;
 pub struct State {
     instance: wgpu::Instance,
@@ -14,6 +15,8 @@ pub struct State {
     device: wgpu::Device,
     queue: wgpu::Queue,
     buffer: wgpu::Buffer,
+    texture: wgpu::Texture,
+    texture_view: wgpu::TextureView,
     width: u32,
     height: u32,
 }
@@ -47,9 +50,9 @@ impl State {
                 trace: wgpu::Trace::Off,
             })
             .await?;
-        let surface_format = wgpu::TextureFormat::Rgba8UnormSrgb;
-        let present_mode = wgpu::PresentMode::AutoNoVsync;
-        let alpha_mode = wgpu::CompositeAlphaMode::Auto;
+        // let surface_format = wgpu::TextureFormat::Rgba8UnormSrgb;
+        // let present_mode = wgpu::PresentMode::AutoNoVsync;
+        // let alpha_mode = wgpu::CompositeAlphaMode::Auto;
 
         let texture_desc = wgpu::TextureDescriptor {
             size: wgpu::Extent3d {
@@ -84,7 +87,126 @@ impl State {
             buffer,
             width,
             height,
+            texture,
+            texture_view,
         })
+    }
+
+    pub async fn draw(&mut self) -> anyhow::Result<()> {
+        // Something something... fragment shader... set colors? >_<
+        let device = &self.device;
+        let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+        let texture_format = self.texture_view.texture().format();
+        let extent = wgpu::Extent3d {
+            width: self.width,
+            height: self.height,
+            depth_or_array_layers: 1,
+        };
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: None,
+                buffers: &[],
+                compilation_options: Default::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: None,
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: texture_format,
+                    blend: Some(wgpu::BlendState {
+                        alpha: wgpu::BlendComponent::REPLACE,
+                        color: wgpu::BlendComponent::REPLACE,
+                    }),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: Default::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            // If the pipeline will be used with a multiview render pass, this
+            // indicates how many array layers the attachments will have.
+            multiview: None,
+            cache: None,
+        });
+
+        let mut encoder =
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+
+        {
+            let render_pass_desc = wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &self.texture_view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: wgpu::StoreOp::Store,
+                    },
+                    depth_slice: None,
+                })],
+                depth_stencil_attachment: None,
+                occlusion_query_set: None,
+                timestamp_writes: None,
+            };
+            let mut render_pass = encoder.begin_render_pass(&render_pass_desc);
+
+            render_pass.set_pipeline(&render_pipeline);
+            render_pass.draw(0..3, 0..1);
+        }
+
+        encoder.copy_texture_to_buffer(
+            wgpu::TexelCopyTextureInfo {
+                aspect: wgpu::TextureAspect::All,
+                texture: &self.texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            wgpu::TexelCopyBufferInfo {
+                buffer: &self.buffer,
+                layout: wgpu::TexelCopyBufferLayout {
+                    offset: 0,
+                    bytes_per_row: Some(self.width * std::mem::size_of::<u32>() as u32),
+                    rows_per_image: Some(self.width),
+                },
+            },
+            extent,
+        );
+        info!("running queue");
+        self.queue.submit(Some(encoder.finish()));
+        Ok(())
     }
 
     pub async fn save<P: AsRef<Path>>(&self, path: P) -> anyhow::Result<()> {
@@ -113,7 +235,8 @@ impl State {
 }
 
 async fn async_main() -> std::result::Result<(), anyhow::Error> {
-    let state = (State::new(64, 64).await)?;
+    let mut state = (State::new(64, 64).await)?;
+    state.draw().await?;
     state.save("/tmp/image.png").await?;
 
     Ok(())
@@ -122,7 +245,8 @@ async fn async_main() -> std::result::Result<(), anyhow::Error> {
 pub fn main() -> std::result::Result<(), anyhow::Error> {
     env_logger::builder()
         .is_test(false)
-        .filter_level(log::LevelFilter::max())
+        .filter_level(log::LevelFilter::Warn)
+        // .filter_level(log::LevelFilter::max())
         .try_init()?;
     pollster::block_on(async_main())?;
     println!("Hello, world! ");
